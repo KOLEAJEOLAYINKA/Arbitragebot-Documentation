739f79d55aff7a90a2ec64177f34580cb0497675
KOLEAJEOLAYINKA <koleajeolayinka@gmail.com>
2022-10-20 14:03:31 +0100

    feat: conf.py change object detailed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'GingerBread-Documetation'
-copyright = '2022, Graziella'
-author = 'Graziella'
+project = 'GingerBread Documentation'
+copyright = '2022, ShootFish'
+author = 'ShootFish'
 
 release = '0.1'
 version = '0.1.0'
